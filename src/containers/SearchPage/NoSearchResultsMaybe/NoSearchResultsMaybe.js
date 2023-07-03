593755a4966e16864b0b200ce39ba005054f36fd
“stewiethepro” <petersloan1984@gmail.com>
2023-07-03 23:23:07 +1200

    add cta to nosearchresultsmaybe on search page

--- src/containers/SearchPage/NoSearchResultsMaybe/NoSearchResultsMaybe.js
+++ src/containers/SearchPage/NoSearchResultsMaybe/NoSearchResultsMaybe.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FormattedMessage } from '../../../util/reactIntl';
+import { NamedLink } from '../../../components';
 import css from './NoSearchResultsMaybe.module.css';
 
 const NoSearchResultsMaybe = props => {
@@ -8,10 +9,25 @@ const NoSearchResultsMaybe = props => {
   const hasSearchParams = location.search?.length > 0;
   return hasNoResult ? (
     <div className={css.noSearchResults}>
+      <br />
+      <br />
       <FormattedMessage id={mobile ? "SearchPage.noResultsTitleMobile" : "SearchPage.noResultsTitleDesktop"} />
       <br />
       <br />
-      <FormattedMessage id={mobile ? "SearchPage.noResultsDescriptionMobile" : "SearchPage.noResultsDescriptionDesktop"} />
+      <p>
+        <FormattedMessage id={mobile ? "SearchPage.noResultsDescriptionMobile" : "SearchPage.noResultsDescriptionDesktop"} />
+      </p>
+      <p>
+        Want to see more chargers in your area? Be the first to&nbsp;
+        <NamedLink name="NewListingPage">
+          list a charger
+        </NamedLink>
+        &nbsp;or&nbsp;
+        <NamedLink name="ReferralsPage">
+          invite a friend
+        </NamedLink>
+        &nbsp;to list theirs.
+      </p>
       <br />
       <br />
       {hasSearchParams ? (
